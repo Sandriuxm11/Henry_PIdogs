297feb690e17e00ad64983a1ae55ada225aedf74
Sandriuxm11 <117607814+Sandriuxm11@users.noreply.github.com>
2023-08-06 21:44:00 -0500

    levanto la app de React

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Detail from './views/detail/detail';
+import Form from './views/form/form';
+import Home from './views/home/home';
+import Landing from './views/landing/landing';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+
+      <Landing/>
+      <Home/>
+      <Detail/>
+      <Form/>
+
     </div>
   );
 }
