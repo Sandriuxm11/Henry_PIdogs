16e03f13601af5a8f41cb9d3ec22841be1795a93
Sandriuxm11 <117607814+Sandriuxm11@users.noreply.github.com>
2023-08-07 20:42:51 -0500

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 import Detail from './views/detail/detail';
 import Form from './views/form/form';
 import Home from './views/home/home';
 import Landing from './views/landing/landing';
+import {Route} from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
 
-      <Landing/>
-      <Home/>
-      <Detail/>
-      <Form/>
+      {location.pathname !== "/" && <NavBar />}
+    
+      <Route exact path= "/" component={Landing} />
+      
+      <Route path="/home" render={() => <Home/>} />
+      
+      <Route path="/detail" render={() => <Detail/>} />
 
+      <Route path="/create" render={() => <Form/>} />
+      
     </div>
   );
 }
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+import style from "./NavBar.module.css";
+
+const NavBar = () => {
+    return(
+        <div className={style.NavBar}>
+        
+        <Link to="/home">Home</Link>
+        <Link to="/create">Form</Link>
+
+        </div>
+    )
+};
+
+export default NavBar;
