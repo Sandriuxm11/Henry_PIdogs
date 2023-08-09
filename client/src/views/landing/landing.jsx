ea4cd8d31385e1e458569293041a5e1b033c1031
Sandriuxm11 <117607814+Sandriuxm11@users.noreply.github.com>
2023-08-08 19:09:14 -0500

    Primera vista del Landing

--- client/src/views/landing/landing.jsx
+++ client/src/views/landing/landing.jsx
@@ -1,8 +1,28 @@
+import style from "./landing.module.css";
+import image from "../../imagenes/los-perros-tienen-mucha-informacion-que-desconoces.jpg";
+import {Link} from "react-router-dom";
+
 const Landing = () => {
     return (
-        <>
-        <h1>Esta es la vista del Landing</h1>
-        </>
+        <div className={style.Landing}>
+
+            <h1>¡Bienvenidos a mi página de Perritos ON-LINE!</h1>
+            <hr></hr>
+            
+            <p>Aquí podrás encontrar información sobre tu peludito favorito, conocer más sobre sus características en general, nombre, peso, talla, temperamento, y escoger alguno que más vaya con tu personalidad.</p>
+            <br></br>
+            <div>
+                <img className={style.imagePrincipal} src={image} alt="Caritas de perros felices." /> 
+                <figcaption>Dueños felices, peluditos felices.</figcaption>
+            </div>
+
+            <div>
+                <button>
+                    <Link to="./home">INGRESAR AHORA</Link>
+                </button>
+            </div>
+           
+        </div>
     )
 };
 
